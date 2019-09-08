aac3077af45099499ecbf69eaadcddd391f73f7c
卡卡 <469748208@qq.com>
2019-09-08 16:54:15 +0800

    实体修改

--- DXYKAdminAPI/src/DXYK.Admin.Entity/Dto/Sys/MenuDto.cs
+++ DXYKAdminAPI/src/DXYK.Admin.Entity/Dto/Sys/MenuDto.cs
@@ -6,6 +6,37 @@ namespace DXYK.Admin.Entity.Dto
 {
     public class MenuDto
     {
-
+        ///<summary>
+        /// 唯一标识
+        ///</summary>
+        public virtual long id { get; set; }
+        ///<summary>
+        /// 菜单名称
+        ///</summary>
+        public virtual string title { get; set; }
+        ///<summary>
+        /// 上级id
+        ///</summary>
+        public virtual long? parent_id { get; set; }
+        ///<summary>
+        /// 图标(Css-Class)
+        ///</summary>
+        public virtual string icon { get; set; }
+        ///<summary>
+        /// 菜单类型
+        ///</summary>
+        public virtual string menu_type { get; set; }
+        ///<summary>
+        /// 跳转
+        ///</summary>
+        public virtual string jump { get; set; }
+        ///<summary>
+        /// 排序
+        ///</summary>
+        public virtual int? sort { get; set; }
+        ///<summary>
+        /// 群组id
+        ///</summary>
+        public virtual long? group_id { get; set; }
     }
 }
